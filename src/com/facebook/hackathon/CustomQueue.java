c219ad8761fc4451af273e78800ce307496991ef
singh-bk <singh.bk@gmail.com>
2019-05-14 01:57:06 +0530

    links

--- src/com/facebook/hackathon/CustomQueue.java
+++ src/com/facebook/hackathon/CustomQueue.java
@@ -1,45 +1,56 @@
 package com.facebook.hackathon;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class CustomQueue<E> {
 
 	private int QUEUE_CAPACITY = 10;
-	private int size;
+	
+	private AtomicInteger size;
 
 	private Node<E> head;
 	private Node<E> tail;
-
+	
+	private ReentrantLock putLock = new ReentrantLock();
+	private ReentrantLock getLock = new ReentrantLock();
+	
 	public CustomQueue(int capacity) {
 		QUEUE_CAPACITY = capacity;
-		this.size = 0;
+		this.size = new AtomicInteger(0);
 	}
 
 	public boolean enqueue(E element) {
-		if (size == QUEUE_CAPACITY) {
+		if (size.get() == QUEUE_CAPACITY) {
 			System.out.println("The queue is already full");
 			return false;
 		} else {
 			Node<E> elem = new Node<E>(element);
-			if(size == 0){
+			if(size.get() == 0){
+				putLock.lock();
 				tail = elem;
 				head = elem;
+				putLock.unlock();
 			}else{
+				putLock.lock();
 				tail.next = elem;
 				tail = elem;
+				putLock.unlock();
 			}
-			size++;
+			size.getAndIncrement();
 			return true;
 		}
 	}
 
 	public E dequeue() {
-		if (size == 0) {
+		if (size.get() == 0) {
 			System.out.println("The queue is empty");
 			return null;
 		} else {
 			Node<E> node = head;
 			head = head.next;
-			size--;
+			size.getAndDecrement();
 			return node.item;
 		}
 	}
